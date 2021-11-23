5a85b7966de825f0e432b40c256b4cc4c640d069
moonian <94371106+moonian@users.noreply.github.com>
2021-11-23 14:15:51 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,18 @@
+import pygame
 import pygame as pg
 from Chess import BackEnd
 
 pg.init()  # we do this to initialize all imported pygame modules
-tile_size = 64  # 512 pixels divided by 8 rows
+board_pixel_size = 512
+num_of_rows = 8
+tile_size = board_pixel_size // num_of_rows  # 512 pixels divided by 8 rows
 chess_piece_images = {}  # we use this to store the images of the chess pieces
-
 
 def load_images():
     """
     We are creating a dictionary of callable images so we can easily reference them throughout the code.
     """
-    chess_pieces = ['BR', 'BKn', 'BB', 'BQ', 'BK', 'BB', 'BKn', 'BR', "BP", "WR", "WKn", "WB", "WQ", "WK", "WB", "WKn",
+    chess_pieces = ['BR', 'BN', 'BB', 'BQ', 'BK', 'BB', 'BN', 'BR', "BP", "WR", "WN", "WB", "WQ", "WK", "WB", "WN",
                     "WR", "WP"]
     for piece in chess_pieces:
         chess_piece_images[piece] = pg.transform.scale(pg.image.load("images/" + piece + ".png"),
@@ -18,9 +20,11 @@ def load_images():
 def main():
     # pg.init()
     screen = pg.display.set_mode(
-        (512, 512))  # initializes a screen for display with size 512x512
+        (board_pixel_size, board_pixel_size))  # initializes a screen for display with size 512x512
     screen.fill(pg.Color("white"))
     chess_game = BackEnd.Chess()
+    valid_moves = chess_game.get_valid_moves()
+    move_made = False  # flag variable for when a move is made
     load_images()
     draw_chess_game(screen, chess_game)
     clock = pg.time.Clock()
@@ -33,6 +37,7 @@ def main():
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 running = False
+            # mouse handler
             elif e.type == pg.MOUSEBUTTONDOWN:
                 location = pg.mouse.get_pos()  # stores the (x,y) coordinates of the mouse
                 col = location[
@@ -49,9 +54,22 @@ def main():
                 if len(player_click) == 2: # we only want to make the move if the user clicked 2 separate tiles
                     move = BackEnd.Move(player_click[0], player_click[1], chess_game.board)
                     print(move.get_chess_notation())
-                    chess_game.make_move(move)
+                    if move in valid_moves:
+                        chess_game.make_move(move)
+                        move_made = True
                     selected_square = ()
                     player_click = []
+
+            # key handler
+            elif e.type == pg.KEYDOWN:
+                if e.key == pg.K_u: # undo the move when u is pressed
+                    chess_game.undo_move()
+                    move_made = True # sets this back to True so move_made function works properly
+
+        if move_made: # we do not want to run the logic to get the valid moves on every action. Only when a valid
+                        # move is made. Therefore, we have this IF statement and the move_made variable to control that.
+            valid_moves = chess_game.get_valid_moves()
+            move_made = False
 
         draw_chess_game(screen, chess_game)
         clock.tick(15)
@@ -70,8 +88,8 @@ def draw_board(screen):
     Draws the tiles on the board by looping through 8 rows and columns
     """
     colors = [pg.Color("white"), pg.Color("gray")]
-    for row in range(8):
-        for col in range(8):
+    for row in range(num_of_rows):
+        for col in range(num_of_rows):
             color = colors[((row + col) % 2)]
             pg.draw.rect(screen, color, pg.Rect(col * tile_size, row * tile_size, tile_size,
                                                 tile_size))  # this moves along the columns and rows by using TILE_SIZE
@@ -81,8 +99,8 @@ def draw_pieces(screen, board):
     """
     Draws the pieces on the board by referencing the instance of the Board class from BackEnd
     """
-    for row in range(8):
-        for col in range(8):
+    for row in range(num_of_rows):
+        for col in range(num_of_rows):
             chess_piece = board[row][col]
             if chess_piece != "--":  # we only want to draw a piece on a non-empty tile
                 screen.blit(chess_piece_images[chess_piece],
